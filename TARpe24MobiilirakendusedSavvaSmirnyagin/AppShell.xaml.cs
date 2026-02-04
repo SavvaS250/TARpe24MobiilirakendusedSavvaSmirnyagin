d0afe2f87c3711e1025a2fb66114146b0f3b0b03
opilane <opilane@HP-CZC2349HV1>
2026-02-04 09:22:30 +0200

    Add project files.

---
+++ TARpe24MobiilirakendusedSavvaSmirnyagin/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TARpe24MobiilirakendusedSavvaSmirnyagin
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TARpe24MobiilirakendusedSavvaSmirnyagin/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TARpe24MobiilirakendusedSavvaSmirnyagin
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
